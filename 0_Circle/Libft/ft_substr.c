79582a0811ad4dc1fbdc7a23371d9737bdeb0566
Gitae Kim <gitkim@c2r6s6.42gyeongsan.kr>
2024-10-01 23:04:22 +0900

    libft_substr

--- 0_Circle/Libft/ft_substr.c
+++ 0_Circle/Libft/ft_substr.c
@@ -6,8 +6,27 @@
 /*   By: gitkim <gitkim@student.42gyeongsan.kr      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 22:09:23 by gitkim            #+#    #+#             */
-/*   Updated: 2024/10/01 22:09:38 by gitkim           ###   ########.fr       */
+/*   Updated: 2024/10/01 23:03:15 by gitkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_
+#include <stdlib.h>
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub_str;
+	size_t	i;
+
+	sub_str = (char *)malloc(sizeof(char) * (len));
+	if (sub_str == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len - 1 && s[start])
+	{
+		sub_str[i] = s[start];
+		i++;
+		start++;
+	}
+	sub_str[i] = '\0';
+	return (sub_str);
+}
