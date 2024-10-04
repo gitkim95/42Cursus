f764c4569dd813e0054c683a2bf3015af04dd703
Gitae Kim <gitkim@c2r7s6.42gyeongsan.kr>
2024-10-04 17:48:53 +0900

    trim comp

--- 0_Circle/Libft/ft_strtrim.c
+++ 0_Circle/Libft/ft_strtrim.c
@@ -3,62 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gitkim <gitkim@student42gyeongsan.kr>      +#+  +:+       +#+        */
+/*   By: gitkim <gitkim@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 23:24:13 by gitkim            #+#    #+#             */
-/*   Updated: 2024/10/04 01:20:15 by gitkim           ###   ########.fr       */
+/*   Updated: 2024/10/04 15:35:37 by gitkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	alloc_result(char const *s1, char const *set, char **result)
+void	trim_paste(char const *s1, char *result, int offset)
 {
-	int	s1_idx;
-	int	set_idx;
-	int	cnt;
-
-	s1_idx = 0;
-	cnt = 0;
-	while (s1[s1_idx])
-	{
-		set_idx = 0;
-		while (set[set_idx])
-		{
-			if (s1[s1_idx] == set[set_idx])
-				break ;
-			set_idx++;
-		}
-		if (s1[s1_idx] != set[set_idx])
-			cnt++;
-		s1_idx++;
-	}
-	*result = (char *)malloc(sizeof(char) * (cnt + 1));
-}
-
-void	trim_paste(char const *s1, char const *set, char *result)
-{
-	int	s1_idx;
-	int	set_idx;
 	int	i;
 
 	i = 0;
-	s1_idx = 0;
-	while (s1[s1_idx])
+	while (*s1 && i < offset)
 	{
-		set_idx = 0;
-		while (set[set_idx])
-		{
-			if (s1[s1_idx] == set[set_idx])
-				break ;
-			set_idx++;
-		}
-		if (s1[s1_idx] != set[set_idx])
-		{
-			result[i] = s1[s1_idx];
-			i++;
-		}
-		s1_idx++;
+		result[i] = *s1;
+		i++;
+		s1++;
 	}
 	result[i] = '\0';
 }
@@ -66,11 +29,18 @@ void	trim_paste(char const *s1, char const *set, char *result)
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*result;
+	int		front;
+	int		back;
 
-	result = NULL;
-	alloc_result(s1, set, &result);
+	front = 0;
+	back = ft_strlen(s1);
+	while (ft_strchr(set, s1[front]) && front < back)
+		front++;
+	while (ft_strchr(set, s1[back - 1]) && front < back)
+		back--;
+	result = (char *)malloc(back - front + 1);
 	if (result == NULL)
 		return (NULL);
-	trim_paste(s1, set, result);
+	trim_paste(s1 + front, result, back - front);
 	return (result);
 }
