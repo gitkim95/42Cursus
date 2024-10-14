92ddf38797bb284d0b985c4e98676f17c819a8ea
Gitae Kim <gitkim@c2r8s8.42gyeongsan.kr>
2024-10-14 18:43:39 +0900

    Libft setting

---
+++ Libft/ft_strchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gitkim <gitkim@student42gyeongsan.kr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 19:08:28 by gitkim            #+#    #+#             */
+/*   Updated: 2024/10/07 12:00:04 by gitkim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	char	*str;
+
+	if (!s)
+		return (NULL);
+	str = (char *)s;
+	while (*str)
+	{
+		if (*str == (char)c)
+			return (str);
+		str++;
+	}
+	if (*str == c)
+		return (str);
+	return (NULL);
+}
