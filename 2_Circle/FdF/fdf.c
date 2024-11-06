2eb5eff5ab004084deb163bb88b43299dfe2596c
gitkim95 <qsawed1003@gmail.com>
2024-11-06 12:59:09 +0900

    valid

--- 2_Circle/FdF/fdf.c
+++ 2_Circle/FdF/fdf.c
@@ -6,13 +6,46 @@
 /*   By: gitkim <gitkim@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 23:06:20 by gitkim            #+#    #+#             */
-/*   Updated: 2024/11/06 03:12:09 by gitkim           ###   ########.fr       */
+/*   Updated: 2024/11/06 12:58:39 by gitkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 #include <mlx.h>
 #include "./libft/libft.h"
+
+void	free_split(char **split)
+{
+	int	idx;
+
+	idx = 0;
+	while (split[idx])
+	{
+		free(split[idx]);
+		idx++;
+	}
+	free(split);
+}
+
+int	valid_fdf_file(char *file_path)
+{
+	char	**file_path_split;
+	int		idx;
+
+	file_path_split = ft_split(file_path, '.');
+	if (!file_path_split)
+		terminator(1, NULL, NULL, NULL);
+	idx = 0;
+	while (file_path_split[idx])
+		idx++;
+	if (!ft_strncmp(file_path_split[idx - 1], "fdf", 4))
+	{
+		free_split(file_path_split);
+		return (1);
+	}
+	else
+		terminator(1, NULL, file_path_split, NULL);
+}
 
 int	main(int ac, char *av[])
 {
