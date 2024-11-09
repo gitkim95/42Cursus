a338197f24877fbeb808f039478e926f6c704e99
Gitae Kim <gitkim@c2r19s6.42gyeongsan.kr>
2024-11-09 22:47:46 +0900

    pipex set

--- 2_Circle/pipex/pipex.c
+++ 2_Circle/pipex/pipex.c
@@ -6,20 +6,122 @@
 /*   By: gitkim <gitkim@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 19:37:57 by gitkim            #+#    #+#             */
-/*   Updated: 2024/11/08 23:21:29 by gitkim           ###   ########.fr       */
+/*   Updated: 2024/11/09 22:47:31 by gitkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+#include "./libft/libft.h"
 
-int main ()
+char	*find_path(char *envp[])
 {
-	char *const env[] = {
-        "MY_HOME=/home/gitkim/42Cursus",
-        NULL
-    };
+	int		i;
+	char	*path;
+
+	i = 0;
+	while (envp[i])
+	{
+		if (!ft_strncmp("PATH=", envp[i], 5))
+		{
+			path = ft_strdup(envp[i] + 5);
+			if (!path)
+			{
+					//error
+			}
+			return (path);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+char	**set_path(char *envp[])
+{
+	char	*path_arr;
+	char	**path_split;
+
+	path_arr = find_path(envp);
+	if (!path_arr)
+	{
+		//error
+	}
+	path_split = ft_split(path_arr, ':');
+	if (!path_split)
+	{
+		//error
+	}
+	return (path_split);
+}
+
+char	*check_cmd(t_pipex *data, char *cmd)
+{
+	int		i;
+	char	*temp;
+	char	*new_cmd;
+
+	if (access(cmd, X_OK) == 0);
+		return (cmd);
+	temp = ft_strjoin("/", cmd);
+	if (!temp)
+	{
+			//error
+	}
+	i = 0;
+	while (data->path[i])
+	{
+		new_cmd = ft_strjoin(data->path[i], temp);
+		if (!new_cmd)
+		{
+			//error
+		}
+		if (access(new_cmd, X_OK) == 0);
+		{
+			free(temp);
+			return (new_cmd);
+		}
+		free(new_cmd);
+		i++;
+	}
+	free(temp);
+	return (NULL);
+}
+
+char	*set_cmd(t_pipex *data, char *cmd)
+{
+	char	*temp_cmd;
+
+	temp_cmd = cmd;
+	cmd = check_cmd(data, cmd);
+	if (cmd != temp_cmd)
+	{
+		free(temp_cmd);
+		if (!cmd)
+		{
+			//error
+		}	
+	}
+	return (cmd);
+}
+
+void	set_data(t_pipex *data, int argc, char *argv[], char *envp[])
+{
+	char	*temp_cmd;
 	
-	char *catArgv[] = {"./exectest", NULL};
-	execve(catArgv[0], catArgv, env);
-	return 0;
+	data->cmd1 = ft_split(argv[2], ' ');
+	data->cmd2 = ft_split(argv[3], ' ');
+	data->path = set_path(envp);
+	data->cmd_idx = argc - 3;
+	data->cmd1[0] = set_cmd(data, data->cmd1[0]);
+	data->cmd2[0] = set_cmd(data, data->cmd2[0]);
+}
+
+int main (int argc, char *argv[], char *envp[])
+{
+	t_pipex	data;
+
+	if (argc != 5)
+	{
+		//error
+	}
+	set_data(&data, argc, argv, envp);
 }
--- 2_Circle/pipex/pipex.h
+++ 2_Circle/pipex/pipex.h
@@ -6,7 +6,7 @@
 /*   By: gitkim <gitkim@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 19:38:04 by gitkim            #+#    #+#             */
-/*   Updated: 2024/11/08 23:52:05 by gitkim           ###   ########.fr       */
+/*   Updated: 2024/11/09 21:32:35 by gitkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,15 @@
 # include <stdio.h> //perror
 # include <string.h> //strerror
 # include <sys/wait.h> // wait, waitpid
+# include <sys/types.h> // pid_t 타입 사용
+# include <errno.h> // errno
 
+typedef struct s_pipex
+{
+	char	**cmd1;
+	char	**cmd2;
+	char	*path;
+}	t_pipex;
 
 #endif
 /*
@@ -30,8 +38,8 @@ cmd2: 두 번째 셸 명령어 및 그에 따른 파라미터
 file2: 두 번째 파일 이름 (출력 파일)
 $> < file1 cmd1 | cmd2 > file2와 같이 작동하게 하는 게 목표.
 프로그램에서 오류 처리를 철저히 해야 하며, 절대로 예기치 않게 종료되지 않도록 해야 한다.
-access#include <unistd.h>
-int access(const char *pathname, int mode);
+
+access(const char *pathname, int mode);
 	- 권한을 확인함
 	- R_OK : 파일 존재 여부, 읽기 권한 여부
 	- W_OK : 파일 존재 여부, 쓰기 권한 여부
@@ -53,8 +61,8 @@ fork()
 	- https://codetravel.tistory.com/23
 	- <sys/types.h> 확인 필요 ----pid_t pid;----
 pipe(int fd[2]);
-	- 함수 호출 후 fd[0]에 데이터 입력을 받을 수 있는 fd 저장 (출구, write)
-	- 함수 호출 후 fd[1]에 데이터 출력을 할 수 있는 fd 저장 (입구, read)
+	- 함수 호출 후 fd[0]에 데이터 입력을 받을 수 있는 fd 저장 (출구, read)
+	- 함수 호출 후 fd[1]에 데이터 출력을 할 수 있는 fd 저장 (입구, write)
 	- 성공 시 0을 반환, 실패 시 -1을 반환
 	- https://nroses-taek.tistory.com/139
 unlink(const char* pathname);
@@ -62,7 +70,26 @@ unlink(const char* pathname);
 	- 성공 시 0을 반환, 실패 시 -1을 반환
 perror(const char *string);
 	- 받은 string을 stderr로 출력
-strerror-
-wait	-
-waitpid	-
+strerror(int errnum);
+	- errno를 전달하여 호출 시 해당 errno에 맞는 오류 메세지를 반환
+	- error에 맞는 메세지가 없을 경우 "Unknown error nnn" 메세지를 반환
+wait(int *wstatus)
+	- wstatus에는 자식 프로세스가 종료될 때의 상태 정보가 저장
+	- 정상 종료 시
+		- 상위 8비트에는 프로세스를 종료시킨 exit 함수의 인수가 기록
+		- 하위 8비트에는 0이 저장 
+	- 비정상 종료 시
+		- 상위 8비트에는 0이 저장
+		- 하위 8비트에는 프로세스를 종료시킨 시그널의 번호가 기록
+	- 성공 시 종료된 자식 프로세스의 PID를 반환, 실패 시 -1을 반환
+waitpid(pid_t pid, int *wstatus, int option)
+	- 첫 번째 인수는 목표 프로세스 타겟이라고 볼 수 있다.
+		- -1을 전달 시 여러 자식 프로세스가 생성이 됐다고 가정 시
+			뭐든 하나라도 종료가 되면 해당 종료된 자식 프로세스의 정보를 받을 수 있다.
+	- 두 번째 인수가 필요 없다면 NULL을 넣어줄 수 있다.
+	- 세 번째 인수는 옵션을 넣어줄 수 있다.
+		- 0을 넣을 시 일반 wait 함수와 같도록 할 수 있다.
+		- WNOHANG을 넣을 시 부모 프로세스가 기다리지 않게 해준다.
+	- 성공 시 종료된 자식 프로세스의 PID를 반환, 실패 시 -1을 반환
+	- https://blog.naver.com/skout123/50133478563
 */
