83b891a52499e96d35b79d2ffabcb18faa267309
Gitae Kim <gitkim@c2r10s5.42gyeongsan.kr>
2024-11-28 19:19:31 +0900

    ~

--- 3_Circle/Philosophers/philo/ph_thread_task.c
+++ 3_Circle/Philosophers/philo/ph_thread_task.c
@@ -6,7 +6,7 @@
 /*   By: gitkim <gitkim@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 22:27:15 by gitkim            #+#    #+#             */
-/*   Updated: 2024/11/28 16:45:59 by gitkim           ###   ########.fr       */
+/*   Updated: 2024/11/28 19:19:20 by gitkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,10 +61,7 @@ int	get_fork(t_philo *philo, t_data *data, int fork_num)
 		else
 			return (0);
 	}
-	pthread_mutex_lock(&(data->fork[fork_num]));
-	pthread_mutex_lock(&(data->ff_mutex[fork_num]));
-	data->fork_flag[fork_num] = philo->ord + 1;
-	pthread_mutex_unlock(&(data->ff_mutex[fork_num]));
+	set_mtx_value(&(data->fork[fork_num]), philo->ord + 1);
 	ph_print_status(data, philo->ord, "has taken a fork");
 	return (1);
 }
@@ -82,13 +79,12 @@ int	ph_philo_think(t_philo *philo, t_data *data)
 
 int	ph_philo_eat(t_philo *philo, t_data *data)
 {
-	pthread_mutex_lock(&(philo->df_mutex));
-	if (philo->dead_flag == 1)
+	if (get_mtx_value(&(philo->dead_flag)))
 		return (0);
-	pthread_mutex_unlock(&(philo->df_mutex));
-	philo->last_time_eaten = ph_get_time();
+	set_mtx_value(&(philo->last_time_eaten), ph_get_time());
 	ph_print_status(data, philo->ord, "is eating");
-	wait_tasking(ph_get_time(), data->time_to_eat);
+	wait_tasking(ph_get_time(), data->time_to_eat); // ttd, tte, tts 중복??
+	set_mtx_value(&(philo->num_of_eaten), )
 	philo->num_of_eaten++;
 	pthread_mutex_unlock(&(data->fork[philo->right_fork]));
 	pthread_mutex_lock(&(data->ff_mutex[philo->right_fork]));
