ce1cab73b169b3e9bd1eb9b7cdb71553ea3654df
Gitae Kim <gitkim@c3r13s4.42gyeongsan.kr>
2025-01-02 22:09:29 +0900

    0203

--- 4_Circle/CPP_Module/02/ex03/Point.hpp
+++ 4_Circle/CPP_Module/02/ex03/Point.hpp
@@ -6,7 +6,7 @@
 /*   By: gitkim <gitkim@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 21:15:57 by gitkim            #+#    #+#             */
-/*   Updated: 2025/01/02 21:28:57 by gitkim           ###   ########.fr       */
+/*   Updated: 2025/01/02 22:09:15 by gitkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,11 @@ private:
 	const Fixed	y;
 public:
 	Point( void );
-	Point( const float a, const float b);
+	Point( const float a, const float b );
 	Point( const Point& other);
 	~Point( void );
 
-	Point&	operator=(const Point& other);
+	Point&	operator=( const Point& other );
 
 	bool	bsp( Point const a, Point const b, Point const c, Point const point);
 };
