bae69dfd276f4d6d05ed0545a58be863be9753fb
Gitae Kim <gitkim@c3r13s6.42gyeongsan.kr>
2025-01-12 18:58:07 +0900

    0401_ing

---
+++ 4_Circle/CPP_Module/04/ex01/WrongAnimal.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   WrongAnimal.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gitkim <gitkim@student.42gyeongsan.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/08 03:44:43 by gitkim            #+#    #+#             */
+/*   Updated: 2025/01/12 18:18:52 by gitkim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WRONGANIMAL_HPP
+# define WRONGANIMAL_HPP
+
+# include <string>
+
+class WrongAnimal
+{
+protected:
+	std::string	type;
+public:
+	WrongAnimal( void );
+	WrongAnimal( const WrongAnimal& other );
+	virtual ~WrongAnimal();
+
+	WrongAnimal&					operator=( const WrongAnimal& other );
+	friend std::ostream&	operator<<( std::ostream& out, const WrongAnimal& WrongAnimal );
+
+	virtual void	makeSound( void ) const;
+	std::string		getType( void ) const;
+};
+
+#endif
