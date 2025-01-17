ca425edfc6d957c5b4af15b322ef903cb9723767
Gitae Kim <gitkim@c3r12s3.42gyeongsan.kr>
2025-01-17 18:29:35 +0900

    C++_05_00_basic_build

--- 5_Circle/CPP_Module/05/ex00/Bureaucrat.cpp
+++ 5_Circle/CPP_Module/05/ex00/Bureaucrat.cpp
@@ -6,58 +6,54 @@
 /*   By: gitkim <gitkim@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 22:00:52 by gitkim            #+#    #+#             */
-/*   Updated: 2025/01/16 22:02:21 by gitkim           ###   ########.fr       */
+/*   Updated: 2025/01/17 18:21:37 by gitkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include "Bureaucrat.hpp"
 
-Bureaucrat::Bureaucrat( void )
-{
+Bureaucrat::Bureaucrat( void ) : name("unnamed"), grade(0) {}
 
-}
+Bureaucrat::Bureaucrat( std::string name, int grade ) : name(name), grade(grade) {}
 
-Bureaucrat::Bureaucrat( std::string name, int grade )
-{
+Bureaucrat::Bureaucrat( const Bureaucrat& other ) : name(other.name), grade(other.grade) {}
 
-}
-
-Bureaucrat::Bureaucrat( const Bureaucrat& other )
-{
-
-}
-
-Bureaucrat::~Bureaucrat( void )
-{
-
-}
+Bureaucrat::~Bureaucrat( void ) {}
 
 Bureaucrat&	Bureaucrat::operator=( const Bureaucrat& other )
 {
-
+	if (this != &other)
+	{
+		grade = other.grade;
+	}
+	return (*this);
 }
 
 std::ostream&	operator<<( std::ostream& out, const Bureaucrat& bureaucrat )
 {
-
+	out	<< bureaucrat.name
+		<< ", bureaucrat grade "
+		<< bureaucrat.grade << std::endl;
+	return (out);
 }
 
 void	Bureaucrat::increaseGrade( void )
 {
-
+	this->grade--;
 }
 
 void	Bureaucrat::decreaseGrade( void )
 {
-
+	this->grade++;
 }
 
 std::string	Bureaucrat::getName( void ) const
 {
-
+	return (name);
 }
 
 int	Bureaucrat::getGrade( void ) const
 {
-
+	return (grade);
 }
