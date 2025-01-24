b59bfdc2f8f34f08f0dd4b0f778b96e71c1fd36b
Gitae Kim <gitkim@c1r20s5.42gyeongsan.kr>
2025-01-25 01:38:10 +0900

    07_start

---
+++ 5_Circle/CPP_Module/07/ex00/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gitkim <gitkim@student.42gyeongsan.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/25 01:35:49 by gitkim            #+#    #+#             */
+/*   Updated: 2025/01/25 01:36:12 by gitkim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int main( void )
+{
+    int a = 2;
+    int b = 3;
+    ::swap(a, b);
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+    return 0;
+}
