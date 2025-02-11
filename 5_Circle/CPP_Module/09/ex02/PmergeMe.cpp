71d1b7507fb03edf72750a300738b71b76ad8093
gitkim95 <qsawed1003@gmail.com>
2025-02-12 04:16:44 +0900

    feat: CPP_0902_vector

--- 5_Circle/CPP_Module/09/ex02/PmergeMe.cpp
+++ 5_Circle/CPP_Module/09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: gitkim <gitkim@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 00:31:00 by gitkim            #+#    #+#             */
-/*   Updated: 2025/02/09 00:32:05 by gitkim           ###   ########.fr       */
+/*   Updated: 2025/02/12 04:11:28 by gitkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,3 +31,72 @@ PmergeMe&	PmergeMe::operator=( const PmergeMe &other )
 {
 	
 }
+
+void	PmergeMe::insertionSort( std::vector<int>& vec, int start, int end)
+{
+	for (int i = start + 1; i <= end; i++)
+	{
+		int	key = vec[i];
+		int	j = i;
+
+		while (j >= start && vec[j] > key)
+		{
+			vec[j + 1] = vec[j];
+			j--;
+		}
+		vec[j + 1] = key;
+	}
+}
+
+void	PmergeMe::insertionSort( std::list<int>& list, int start, int end)
+{
+	
+}
+
+void	PmergeMe::mergeSort( std::vector<int>& vec, int start, int mid, int end)
+{
+	std::vector<int>	left(vec.begin() + start, vec.begin() + mid + 1);
+	std::vector<int>	right(vec.begin() + mid + 1, vec.begin() + end + 1); 
+	int					leftIdx = 0, rightIdx = 0, vecIdx = start;
+	
+	while (leftIdx < left.size() && rightIdx < right.size())
+	{
+		if (left[leftIdx] < right[rightIdx])
+			vec[vecIdx++] = left[leftIdx++];
+		else
+			vec[vecIdx++] = right[rightIdx++];
+	}
+	while (leftIdx < left.size())
+		vec[vecIdx++] = left[leftIdx++];
+	while (rightIdx < right.size())
+		vec[vecIdx++] = right[rightIdx++];
+}
+
+
+void	PmergeMe::mergeSort( std::list<int>& list, int start, int mid, int end)
+{
+
+
+}
+
+void	PmergeMe::mergeInsertionSort( std::vector<int>& vec, int start, int end)
+{
+	int	targetSize = 5;
+	if (end - start + 1 <= targetSize)
+	{
+		insertionSort(vec, start, end);
+		return ;
+	}
+
+	int	mid = (end - start) / 2 + start;
+	mergeInsertionSort(vec, start, mid);
+	mergeInsertionSort(vec, mid + 1, end);
+	mergeSort(vec, start, mid, end);
+}
+
+
+void	PmergeMe::mergeInsertionSort( std::list<int>& list, int start, int end)
+{
+
+}
+
--- 5_Circle/CPP_Module/09/ex02/PmergeMe.hpp
+++ 5_Circle/CPP_Module/09/ex02/PmergeMe.hpp
@@ -6,23 +6,34 @@
 /*   By: gitkim <gitkim@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 00:29:48 by gitkim            #+#    #+#             */
-/*   Updated: 2025/02/09 00:30:56 by gitkim           ###   ########.fr       */
+/*   Updated: 2025/02/12 03:14:37 by gitkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PMERGEME_HPP
 # define PMERGEME_HPP
 
+# include <vector>
+# include <list>
+
 class PmergeMe
 {
 private:
-	/* data */
+	std::vector<int>	vec;
+	std::list<int>		list;
 public:
 	PmergeMe( void );
 	PmergeMe( const PmergeMe &other);
 	~PmergeMe( void );
 
 	PmergeMe&	operator=( const PmergeMe &other );
+	
+	void	insertionSort( std::vector<int>& vec, int start, int end);
+	void	insertionSort( std::list<int>& list, int start, int end);
+	void	mergeSort( std::vector<int>& vec, int start, int mid, int end);
+	void	mergeSort( std::list<int>& list, int start, int mid, int end);
+	void	mergeInsertionSort( std::vector<int>& vec, int start, int end);
+	void	mergeInsertionSort( std::list<int>& list, int start, int end);
 };
 
 #endif
