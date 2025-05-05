8649fd7d9bc8f873ab92c2737c2660140bc1c33c
gitkim95 <qsawed1003@gmail.com>
2025-05-05 21:09:52 +0900

    fix: init_domain 2

--- 5_Circle/Inception/init_domain.sh
+++ 5_Circle/Inception/init_domain.sh
@@ -6,16 +6,15 @@ read ip
 add_host_entry() {
     local domain=$1
 
-    # /etc/hosts 안에 이 도메인이 이미 있는지 검사
-    if grep -qE "\s${domain}(\s|$)" /etc/hosts; then
-        echo "$domain exists!"
-    else
-        echo "$domain created."
-        echo "$ip $domain" | sudo tee -a /etc/hosts > /dev/null
-    fi
+    # 기존 줄에서 해당 도메인이 포함된 줄을 삭제 (정확히 포함된 줄만)
+    sudo sed -i "/[[:space:]]\+$domain\([[:space:]]\+\|$\)/d" /etc/hosts
+
+    # 도메인 추가
+    echo "$ip $domain" | sudo tee -a /etc/hosts > /dev/null
+    echo "$domain added with IP $ip"
 }
 
-# 추가하고 싶은 도메인들
+# 도메인들 추가
 add_host_entry gitkim.42.fr
 add_host_entry adminer.gitkim.42.fr
 add_host_entry static.gitkim.42.fr
