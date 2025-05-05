e94a9562187e2c3957dd101534702db1c496c24c
gitkim95 <qsawed1003@gmail.com>
2025-05-05 20:58:45 +0900

    fix: init_domain

--- 5_Circle/Inception/init_domain.sh
+++ 5_Circle/Inception/init_domain.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
 echo "### Enter the server IP ###"
-read ip;
+read ip
 
-if grep -q -E "^gitkim.42.fr$" /etc/hosts; then
-    echo "gitkim.42.fr exist !"
-else
-    echo "gitkim.42.fr create "
-    echo "$ip gitkim.42.fr" >> /etc/hosts
-fi
+add_host_entry() {
+    local domain=$1
 
-if grep -q "adminer.gitkim.42.fr" /etc/hosts; then
-    echo "adminer.gitkim.42.fr exist !"
-else
-    echo "adminer.gitkim.42.fr create "
-    echo "$ip adminer.gitkim.42.fr" >> /etc/hosts
-fi
+    # /etc/hosts 안에 이 도메인이 이미 있는지 검사
+    if grep -qE "\s${domain}(\s|$)" /etc/hosts; then
+        echo "$domain exists!"
+    else
+        echo "$domain created."
+        echo "$ip $domain" | sudo tee -a /etc/hosts > /dev/null
+    fi
+}
 
-if grep -q "static.gitkim.42.fr" /etc/hosts; then
-    echo "static.gitkim.42.fr exist !"
-else
-    echo "static.gitkim.42.fr create "
-    echo "$ip static.gitkim.42.fr" >> /etc/hosts
-fi
+# 추가하고 싶은 도메인들
+add_host_entry gitkim.42.fr
+add_host_entry adminer.gitkim.42.fr
+add_host_entry static.gitkim.42.fr
