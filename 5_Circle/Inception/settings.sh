417d40308d2438c9067b66212e730b26a7a6f2a6
Gitae Kim <gitkim@c3r11s4.42gyeongsan.kr>
2025-05-06 22:17:16 +0900

    fix: passwords and static-pages

--- 5_Circle/Inception/settings.sh
+++ 5_Circle/Inception/settings.sh
@@ -5,7 +5,7 @@ apt upgrade -y
 apt install sudo
 echo 'export PATH=$PATH:/sbin:/usr/sbin:/usr/local/sbin:/bin:/usr/bin:/usr/local/bin:$HOME/bin:$HOME/.local/bin:$HOME/go/bin:$HOME/.cargo/bin' >> ~/.bashrc
 source ~/.bashrc
-apt install -y ca-certificates curl git make vim docker docker-compose ufw wget
+apt install -y ca-certificates curl git make vim docker docker-compose ufw wget filezilla
 systemctl start ufw
 ufw enable
 ufw allow 22/tcp
