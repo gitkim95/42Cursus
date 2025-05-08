214a7c860380df02df4080a2d9f7e75d92b46331
Gitae Kim <gitkim@c3r12s1.42gyeongsan.kr>
2025-05-08 23:33:57 +0900

    select loop

--- practice/6_1t/mini_serv.c
+++ practice/6_1t/mini_serv.c
@@ -7,19 +7,18 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+char print_buf[120000];
+char read_buf[120000];
+fd_set fdsets, readfds;
+int maxfd = 0;
+
 void print_error_and_exit(const char *msg)
 {
-	char print_buf[1024];
 	int len;
 
 	len = sprintf(print_buf, "%s\n", msg);
 	write(2, print_buf, len);
 	exit(1);
-}
-
-short swap_bits(short value)
-{
-	return ((value & 0x00FF) << 8) | ((value & 0xFF00) >> 8);
 }
 
 int main(int argc, char *argv)
@@ -45,7 +44,7 @@ int main(int argc, char *argv)
 	bzero(&serv_addr, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = INADDR_LOOPBACK;
-	serv_addr.sin_port = swap_bits(port);
+	serv_addr.sin_port = htons(port);
 
 	// socket bind
 	if (bind(servfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
@@ -61,13 +60,37 @@ int main(int argc, char *argv)
 		print_error_and_exit("Fatal error");
 	}
 
-	// socket accept
-	struct sockaddr_in cli_addr;
-	socklen_t cli_len = sizeof(cli_addr);
-	int clifd = accept(servfd, (struct sockaddr *)&cli_addr, &cli_len);
-	if (clifd < 0)
+	maxfd = servfd;
+	FD_ZERO(&fdsets);
+	FD_SET(servfd, &fdsets);
+	while (1)
 	{
-		close(servfd);
-		print_error_and_exit("Fatal error");
+		readfds = fdsets;
+		if (select(maxfd + 1, &readfds, NULL, NULL, NULL) < 0)
+			print_error_and_exit("Fatal error");
+		for (int fd = 0; fd <= maxfd; ++fd)
+		{
+			if (FD_ISSET(fd, &readfds))
+			{
+				if (fd == servfd)
+				{
+					struct sockaddr_in cli_addr;
+					socklen_t cli_len = sizeof(cli_addr);
+					int clifd = accept(servfd, (struct sockaddr *)&cli_addr, &cli_len);
+					if (clifd < 0)
+					{
+						close(servfd);
+						print_error_and_exit("Fatal error");
+					}
+
+					FD_SET(clifd, &fdsets);
+					if (clifd > maxfd)
+						maxfd = clifd;
+				}
+				else
+				{
+				}
+			}
+		}
 	}
 }
